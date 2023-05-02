cf2c3fa480d43b8c2baaed68265fa63bfd522829
mohammadIsamil <ismailalam901@gmail.com>
2023-05-02 21:45:17 +0600

    Dashboard btn added

--- src/Pages/Dashboard.js
+++ src/Pages/Dashboard.js
@@ -1,10 +1,47 @@
-import React from 'react';
-import myImage from '../Assets/img/hl3.jpg';
+import React from "react";
+import backgroundImage from "../Assets/img/hl3.jpg";
+import { Button } from "@mui/material";
 
 export default function Dashboard() {
   return (
-    <div style={{height: '100vh', width: '100vw', backgroundImage: `url(${myImage})`, backgroundSize: 'cover', backgroundPosition: 'center'}}>
-      {/* content here */}
+    <div
+      className="bg-img"
+      style={{
+        height: "100vh",
+        width: "100vw",
+        backgroundImage: `url(${backgroundImage})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        display: "flex",
+        justifyContent: "flex-end",
+      }}
+    >
+      <div className="dashboard-btn">
+        <Button
+          variant="contained"
+          sx={{ backgroundColor: "#444444", color: "white" }}
+        >
+          Room Details
+        </Button>
+        <Button
+          variant="contained"
+          sx={{ backgroundColor: "#444444", color: "white" }}
+        >
+          Sign Up
+        </Button>
+        <Button
+          variant="contained"
+          sx={{ backgroundColor: "#444444", color: "white" }}
+        >
+          Log In
+        </Button>
+        <Button
+          variant="contained"
+          sx={{ backgroundColor: "#444444", color: "white" }}
+        >
+          Contact
+        </Button>
+      </div>
     </div>
   );
 }
